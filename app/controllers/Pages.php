bcfe831d3a21df7948acef9ca84a0e65c3db4564
Jürgen Sepp <jurgen.sepp@khk.ee>
2021-04-05 14:32:29 +0300

    Määratud ja võetud kasutusele kontrolleri meetod ning võimalikud parameetrid

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -9,4 +9,12 @@ class Pages
     public function __construct() {
         echo 'Pages class is loaded<br>';
     }
+
+    public function index() {
+        echo 'index method is loaded<br>';
+    }
+
+    public function about() {
+        echo 'about method is loaded<br>';
+    }
 }
