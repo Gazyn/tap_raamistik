acbbbb200a4f9d5b5206afcb134bee6622e896ce
Jürgen Sepp <jurgen.sepp@khk.ee>
2021-04-26 09:06:05 +0300

    Lisatud kasutusule rakendusega seotud konstandid

---
+++ app/config/constants.php
@@ -0,0 +1,7 @@
+<?php
+// Project name
+define('PROJECT_NAME', 'tap');
+// App root dir
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL root
+define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
